03840cb3a736a1de85fc3ea44c555ba910545780
Fabian295 <fabiankanhai70@gmail.com>
2023-01-01 09:00:11 +0100

    Add errorHandler and express-async-handler package

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 console.log('Hello backend mern!');
 const express   = require('express');
 const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware');
 const port = process.env.PORT || 5005;
 
 const app = express();
@@ -8,6 +9,8 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.use('/api/goals', require('./routes/goalRoutes'));
+app.use('/api/goals', require('./routes/goalRoutes'))
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+app.use(errorHandler)
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
