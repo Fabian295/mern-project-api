92b32810bae28353c486283fae4c1fde4b6cc8a1
Fabian295 <fabiankanhai70@gmail.com>
2023-01-01 10:38:30 +0100

    Add possible shorter version. Read comments

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -2,6 +2,7 @@ const express =require('express')
 const router = express.Router()
 const { getGoals, setGoal, updateGoal, deleteGoal } = require('../controllers/goalController')
 
+// Verbose way,  but a bit more clear
 router.get('/', getGoals)
 
 router.post('/', setGoal)
@@ -9,5 +10,12 @@ router.post('/', setGoal)
 router.put('/:id', updateGoal)
 
 router.delete('/:id', deleteGoal)
+
+/**
+ * Shorter way, but a bit less clear
+ * 
+ *      router.route('/').get(protect, getGoals).post(protect, setGoal)
+ *      router.route('/:id').delete(protect, deleteGoal).put(protect, updateGoal)
+ */
 
 module.exports = router;
