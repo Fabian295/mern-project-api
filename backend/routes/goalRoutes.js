2c5195035b00a6bbccded5b295554398b3c46fba
Fabian295 <fabiankanhai70@gmail.com>
2023-01-01 08:55:37 +0100

    Add more endpoints

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,8 +1,13 @@
 const express =require('express')
 const router = express.Router()
+const { getGoals, setGoal, updateGoal, deleteGoal } = require('../controllers/goalController')
 
-router.get('/', (req, res) => {
-  res.status(200).json({message: "From the router. Get your goals"})
-})
+router.get('/', getGoals)
+
+router.post('/', setGoal)
+
+router.put('/:id', updateGoal)
+
+router.delete('/:id', deleteGoal)
 
 module.exports = router;
