f4e43761b4a845b7725c7cddb3fcead70cd4e08e
Fabian295 <fabiankanhai70@gmail.com>
2023-01-10 14:21:28 +0100

    Update

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -41,9 +41,9 @@ export const login = createAsyncThunk('auth/login', async (user, thunkAPI) => {
   }
 })
 
-export const logout = createAsyncThunk('auth/logout', async () => (
+export const logout = createAsyncThunk('auth/logout', async () => {
   await authService.logout()  
-))
+})
 
 export const authSlice = createSlice({
   name: 'auth',
